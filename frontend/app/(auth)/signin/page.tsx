ec419a98b2078601f7ebd813129f657596613be1
jack.dev <jack@hotmail.com>
2026-05-06 00:48:34 -0700

    feat:fix

--- frontend/app/(auth)/signin/page.tsx
+++ frontend/app/(auth)/signin/page.tsx
@@ -1,16 +1,18 @@
 "use client"
 
+import Loading from "@/app/loading"
 import { useSigninController } from "@/components/auth/signin/hooks/use-signin-controller"
 import { SigninPageView } from "@/components/auth/signin/signin-page-view"
 import { SigninTenantDialog } from "@/components/auth/signin/signin-tenant-dialog"
+import { Suspense } from "react"
 
 export default function SigninPage() {
   const { dialog, view } = useSigninController()
 
   return (
-    <>
+    <Suspense fallback={<Loading />}>
       <SigninPageView {...view} />
       <SigninTenantDialog {...dialog} />
-    </>
+    </Suspense>
   )
 }
--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,10 +1,16 @@
 "use client"
 
+import Loading from "@/app/loading"
 import { useSignupController } from "@/components/auth/signup/hooks/use-signup-controller"
 import { SignupPageView } from "@/components/auth/signup/signup-page-view"
+import { Suspense } from "react"
 
 export default function SignupPage() {
   const { view } = useSignupController()
 
-  return <SignupPageView {...view} />
+  return (
+    <Suspense fallback={<Loading />}>
+      <SignupPageView {...view} />
+    </Suspense>
+  )
 }
